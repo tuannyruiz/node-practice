6eeb8a92a38a2203134b3f25cfe24c19bc16db21
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-04-30 18:23:20 -0300

    add mysql

--- app/routes/products.js
+++ app/routes/products.js
@@ -1,5 +1,17 @@
 module.exports = function(app) {
   app.get('/produtos', function(req, res){
-    res.render("produtos/lista");
+	var mysql = require('mysql');
+	var connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'dashboard_nodejs'
+	});
+
+	connection.query('select * from books', function(err, result){
+		res.send(results);
+	});
+
+	connection.end();
   });
 };
